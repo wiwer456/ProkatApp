5ced3815cee5f7946f92d35a7aff8689df1eb8f8
wiwer456 <67992786+wiwer456@users.noreply.github.com>
2025-10-20 13:24:19 +0300

    add

--- ProkatApp/App.axaml.cs
+++ ProkatApp/App.axaml.cs
@@ -15,7 +15,7 @@ namespace ProkatApp
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow();
+                desktop.MainWindow = new Window1();
             }
 
             base.OnFrameworkInitializationCompleted();
